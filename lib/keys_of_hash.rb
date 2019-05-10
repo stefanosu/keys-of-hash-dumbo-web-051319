7ca0e2ed44b751bf39ec7f8f3e943ff3bae96759
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 21:29:09 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
-class Hash
-  def keys_of(arguments)
 
+
+class Hash
+  def keys_of(*arguments)
+    array = []
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+        array.push(key)
+        end
+      end 
+
+
+    end
+    return array
   end
-end
+  
+end 
